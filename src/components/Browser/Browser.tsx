58733682ee65ef22407e133b97dbc7120680cb27
Maciej <schmeterling271@gmail.com>
2025-05-15 01:10:43 +0200

    most functionality is there needs polishing

--- src/components/Browser/Browser.tsx
+++ src/components/Browser/Browser.tsx
@@ -4,7 +4,7 @@ import * as Types from "./Browser.types";
 import * as Shared from "../../shared/Shared.types";
 import { FileCard } from "./File/FileCard";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { IconChevronLeft } from "@tabler/icons-react";
+import { IconChevronLeft, IconSettings } from "@tabler/icons-react";
 
 export const Browser = ({}: Types.BrowserTypes) => {
   const [files, setFiles] = React.useState<Shared.File[]>([]);
@@ -15,11 +15,14 @@ export const Browser = ({}: Types.BrowserTypes) => {
     location.pathname.replace(/^\/files\/?/, "")
   );
 
+  const pathSegments = subPath.split("/").filter(Boolean);
+  const parentPath = pathSegments.slice(0, -1).join("/");
+  const showBackButton = pathSegments.length > 0; // Only show if not at root
+
   const handleClick = (file: Shared.File) => {
     if (file.folder) {
       navigate(`/files/${subPath ? `${subPath}/` : ""}${file.name}`);
     } else {
-      console.log(`http://127.0.0.1:8080${file.path}`);
       window.location.href = `http://127.0.0.1:8080${file.path}`;
     }
   };
@@ -30,8 +33,6 @@ export const Browser = ({}: Types.BrowserTypes) => {
     axios
       .get(baseUrl, { params })
       .then((response) => {
-        console.log(response.data);
-        console.log(params);
         setFiles(response.data);
       })
 
@@ -42,15 +43,27 @@ export const Browser = ({}: Types.BrowserTypes) => {
 
   return (
     <>
-      <div className="bg-gray-200 h-screen dark:bg-stone-800 text-2xl p-2 text-stone-700 dark:text-stone-50 inset-0">
+      <div className="bg-gray-200 h-screen dark:bg-stone-800 text-2xl p-4 text-stone-700 dark:text-stone-50 inset-0">
         <div className="buttons flex gap-4 h-16">
           <div className="p-2 flex items-center justify-center aspect-square w-fit bg-stone-700 text-white rounded-lg hover:bg-stone-600">
-            <Link to="/files">
-              <IconChevronLeft />
-            </Link>
+            {!showBackButton ? (
+              <Link
+                to="/settings"
+                className="w-full h-full flex items-center justify-center"
+              >
+                <IconSettings />
+              </Link>
+            ) : (
+              <Link
+                to="/files"
+                className="w-full h-full flex items-center justify-center"
+              >
+                <IconChevronLeft />
+              </Link>
+            )}
           </div>
           <div className="bg-gray-100 w-full dark:bg-stone-700 shadow-sm rounded-lg p-4">
-            File path
+            Current path: {subPath || "Root"}
           </div>
         </div>
         <div className="folders grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 mt-4">
